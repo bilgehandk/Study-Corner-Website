2a8656f25ed7faf2fef78d675b5798758a700ee2
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-05-01 02:02:22 +0300

    wip

--- JavaScript/pomodoroTimer.js
+++ JavaScript/pomodoroTimer.js
@@ -1,49 +1,79 @@
-
-let workTittle = document.getElementById('work');
-let breakTittle = document.getElementById('break');
+let workTitle = document.getElementById('work');
+let breakTitle = document.getElementById('break');
 
 let workTime = 1;
 let breakTime = 5;
 
 let seconds = "00";
 let myVar;
+let breakCount = 0;
+let workMinutes;
+let breakMinutes;
+let isEditable = false;
 
 //display
-
 window.onload = () => {
     document.getElementById('minutes').innerHTML = workTime;
     document.getElementById('seconds').innerHTML = seconds;
 
-    workTittle.classList.add('active');
+    workTitle.classList.add('active');
+}
+
+//editable minutes
+function editMinutes() {
+    if (!isEditable) {
+        isEditable = true;
+        let minutesElement = document.getElementById('minutes');
+        minutesElement.innerHTML = "<input type='number' id='editableMinutes' min='1' value='" + workTime + "' onchange='setMinutes()'>";
+    }
+}
+
+function setMinutes() {
+    let minutesElement = document.getElementById('editableMinutes');
+    let newWorkTime = minutesElement.value;
+    if (newWorkTime !== "") {
+        workTime = parseInt(newWorkTime);
+        document.getElementById('minutes').innerHTML = workTime;
+    }
+    isEditable = false;
 }
 
 
-function restart()
-{
-document.getElementById('start').style.display = "block";
-document.getElementById('reset').style.display = "none";
-seconds = 59;
-let workMinutes = workTime - 1;
-let breakMinutes = breakTime - 1;
-breakCount = 0;
-document.getElementById('minutes').innerHTML = workMinutes;
-document.getElementById('seconds').innerHTML = seconds;
-clearInterval(myVar);
+
+function restart() {
+    isEditable = false;
+    clearInterval(myVar); // Clear the interval timer
+    document.getElementById('start').style.display = 'block';
+    document.getElementById('reset').style.display = 'none';
+    seconds = 0; // Reset the seconds to '00'
+    workMinutes = workTime; // Reset the work minutes to the original value
+    breakCount = 0; // Reset the break count to 0
+
+    let timerFunction = () => {
+        workTitle.classList.add('active'); // Set the work title to active
+        breakTitle.classList.remove('active'); // Set the break title to inactive
+        document.getElementById('editableMinutes').value = workMinutes;
+        document.getElementById('minutes').innerHTML = workMinutes; // Update the minutes display with the new work minutes
+        document.getElementById('seconds').innerHTML = seconds; // Update the seconds display with the new seconds
+    }
+   
+    myvar = setTimeout(timerFunction, 1000);
 }
+
+  
+  
+  
 
 //start timer
-
-function start()
-{
+function start() {
+    isEditable = true;
     document.getElementById('start').style.display = "none";
     document.getElementById('reset').style.display = "block";
 
     seconds = 59;
 
-    let workMinutes = workTime - 1;
-    let breakMinutes = breakTime - 1;
-
-    breakCount = 0;
+    workMinutes = workTime - 1;
+    breakMinutes = breakTime - 1;
 
     //countdown
     let timerFunction = () => {
@@ -53,27 +83,23 @@ function start()
         //change time
         seconds = seconds - 1;
 
-        if(seconds === 0)
-        {
-            workMinutes = workMinutes -1;
-            if(workMinutes === -1)
-            {
-                if(breakCount % 2 === 0)
-                {
+        if (seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if (workMinutes === -1) {
+                if (breakCount % 2 === 0) {
                     //start break
                     workMinutes = breakMinutes;
                     breakCount++;
 
-                    workTittle.classList.remove('active');
-                    breakTittle.classList.add('active');
-                }
-                else{
+                    workTitle.classList.remove('active');
+                    breakTitle.classList.add('active');
+                } else {
                     //continue
                     workMinutes = workTime;
                     breakCount++;
 
-                    breakTittle.classList.remove('active');
-                    workTittle.classList.add('active');
+                    breakTitle.classList.remove('active');
+                    workTitle.classList.add('active');
                 }
             }
             seconds = 59;
@@ -82,7 +108,4 @@ function start()
 
     // start countdown
     myVar = setInterval(timerFunction, 1000);
-
-
 }
-

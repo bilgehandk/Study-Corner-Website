efd44014416a559f85b3141a532c7f7188b26585
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-05-11 19:48:55 +0300

    todo list bugs fixed

--- JavaScript/weeklyplanner.js
+++ JavaScript/weeklyplanner.js
@@ -4,12 +4,14 @@ const addBtn = document.getElementById('addBtn');
 const datePicker = document.getElementById('datePicker');
 const updateBtn = document.getElementById('updateBtn');
 const weekHeading = document.getElementById('weekHeading');
-const mainTable = document.getElementById('maintable');
+const mainTable = document.getElementsByClassName('maintable');
 
 addBtn.addEventListener('click', () => {
-    if (input.value === '') {
-        alert('Please enter a task!');
+    
+    if (input.value === '' || count === 1) {
+        alert('Please enter a task! or Choose date!');
     } else {
+        console.log(`list${count}`);
         const list = document.getElementById(`list${count}`);
         
         const li = document.createElement('li');
@@ -30,9 +32,17 @@ updateBtn.addEventListener('click', () => {
     const weekDiv = document.createElement('div'); // create new div element
     console.log(weekDiv);
     weekDiv.classList.add('week'); // add the 'week' class to the div
+   
+    const h2 = document.createElement('h2'); // create new unordered list element
+    h2.id = 'weekHeading'+(++count); // set the id of the list element to a new unique value
+    h2.innerText = date.toLocaleDateString(undefined, options);
+   
     const ul = document.createElement('ul'); // create new unordered list element
-    ul.id = `list${++count}`; // set the id of the list element to a new unique value
+    ul.id = `list${count}`; // set the id of the list element to a new unique value
+    weekDiv.appendChild(h2); // add the list element to the new div element
     weekDiv.appendChild(ul); // add the list element to the new div element
-    weekHeading.innerText = date.toLocaleDateString(undefined, options);
-    mainTable.appendChild(weekDiv); 
+    weekDiv.id='count'+count;
+
+    console.log(mainTable[0].innerHTML);
+    mainTable[0].appendChild(weekDiv); 
 });

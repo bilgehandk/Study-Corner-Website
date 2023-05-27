994ee40ce5e769966da613be16e6cf87066329df
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-05-27 23:20:25 +0300

    wip cgpa

--- JavaScript/calculateCGPA.js
+++ JavaScript/calculateCGPA.js
@@ -1,40 +1,76 @@
+let courseList = [];
 let totalCredits = 0;
 let totalPoints = 0;
 
 function calculateCGPA() {
+  const courseInput = document.getElementById('course');
   const gradeInput = document.getElementById('grade');
   const creditInput = document.getElementById('credit');
   const resultDiv = document.getElementById('result');
   const courseListDiv = document.getElementById('courseList');
 
-  const grade = parseFloat(gradeInput.value);
+  const courseName = courseInput.value;
+  const grade = gradeInput.value;
   const credit = parseInt(creditInput.value);
 
-  if (isNaN(grade) || grade < 0 || grade > 4 || isNaN(credit)) {
-    resultDiv.textContent = 'Please enter valid grade and credit hours.';
+  if (courseName.trim() === '') {
+    resultDiv.textContent = 'Please enter a valid course name.';
     return;
   }
 
-  totalPoints += grade * credit;
+  if (isNaN(credit)) {
+    resultDiv.textContent = 'Please select a valid credit hours value.';
+    return;
+  }
+
+  courseList.push({ courseName, grade, credit });
+
+  const courseItem = document.createElement('div');
+  courseItem.innerHTML = `${courseName} - Grade: ${grade} - Credit Hours: ${credit}`;
+  courseListDiv.appendChild(courseItem);
+
+  totalPoints += calculateGradePoints(grade) * credit;
   totalCredits += credit;
 
-  gradeInput.value = '';
+  courseInput.value = '';
+  gradeInput.value = 'A+';
   creditInput.value = '1';
 
-  const cgpa = totalPoints / totalCredits;
-  resultDiv.textContent = 'Your CGPA: ' + cgpa.toFixed(2);
+  const cgpa = calculateCGPAValue();
+  resultDiv.textContent = `Your CGPA: ${cgpa.toFixed(2)}`;
+}
 
-  // Create a new course entry
-  const courseEntry = document.createElement('div');
-  courseEntry.className = 'course-entry';
+function calculateGradePoints(grade) {
+  switch (grade) {
+    case 'A+':
+      return 4.00;
+    case 'A':
+      return 4.00;
+    case 'A-':
+      return 3.70;
+    case 'B+':
+      return 3.30;
+    case 'B':
+      return 3.00;
+    case 'B-':
+      return 2.70;
+    case 'C+':
+      return 2.30;
+    case 'C':
+      return 2.00;
+    case 'C-':
+      return 1.70;
+    case 'D+':
+      return 1.30;
+    case 'D':
+      return 1.00;
+    case 'F':
+      return 0.00;
+    default:
+      return 0.00;
+  }
+}
 
-  const gradeSpan = document.createElement('span');
-  gradeSpan.textContent = ' Grade: ' + grade.toFixed(1);
-  courseEntry.appendChild(gradeSpan);
-
-  const creditSpan = document.createElement('span');
-  creditSpan.textContent = ' Credit Hours: ' + credit;
-  courseEntry.appendChild(creditSpan);
-
-  courseListDiv.appendChild(courseEntry);
+function calculateCGPAValue() {
+  return totalPoints / totalCredits;
 }

8f95bfac6a5df6522789cc79a6f5d0691825f646
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-06-02 18:53:17 +0300

    restart game implemantation

--- JavaScript/BatmanGame.js
+++ JavaScript/BatmanGame.js
@@ -2,6 +2,7 @@ const batman = document.getElementById("batman");
 const sam = document.getElementById("sam");
 let score = 0;
 const scoreElement = document.getElementById("score");
+let gameover = false;
 
 function updateScore() {
   score += 1;
@@ -36,6 +37,33 @@ function reset_animation() {
       });
 }
 
+function continueGame()
+    {
+      reset_animation();
+      resetScore(); // Skoru sıfırla
+      if(gameover == true){
+      var restart = document.getElementById("restart");
+      restart.style.display = 'none';
+      var batman = document.getElementById('batman');
+      batman.style.display = 'block';
+      var joker = document.getElementById("sam");
+      joker.style.display = 'block'; 
+      gameover = false;
+      }
+    }
+
+function showGameOver()
+{
+  var restart = document.getElementById("restart");
+  restart.style.display = 'block';
+  var rest = document.getElementById('sam');
+  rest.style.animationName = "none";
+  var batman = document.getElementById('batman');
+  batman.style.display = 'none';
+  var joker = document.getElementById("sam");
+  joker.style.display = 'none';
+}
+
 let isAlive = setInterval(function () {
   // get current dino Y position
   let batmanTop = parseInt(window.getComputedStyle(batman).getPropertyValue("top"));
@@ -44,12 +72,17 @@ let isAlive = setInterval(function () {
   let samLeft = parseInt(window.getComputedStyle(sam).getPropertyValue("left"));
 
   // detect collision
-  if (samLeft < 50 && samLeft > 0 && batmanTop >= 140) {
+  if (samLeft < 50 && samLeft > 0 && batmanTop >= 196) {
     // collision
-    alert("Game Over!");
-    reset_animation();
-    resetScore(); // Skoru sıfırla
+    gameover = true;
+    if(gameover === true)
+    {
+      showGameOver();
+    }
+    
+    
   } else {
+    if(gameover === false)
     updateScore(); // Skoru güncelle
   }
 }, 10);

92637c52fb00ad9caa45d97e4afcc5fe3204ba32
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2023-06-03 10:00:30 +0300

    wip jquery plug in

--- JavaScript/weeklyplanner.js
+++ JavaScript/weeklyplanner.js
@@ -1,20 +1,22 @@
 var count = 1;
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
-const datePicker = document.getElementById('datePicker');
+const datePicker = $('#datePicker');
 const updateBtn = document.getElementById('updateBtn');
 const weekHeading = document.getElementById('weekHeading');
 const mainTable = document.getElementsByClassName('maintable');
 const li = document.createElement('li');
 
+// Initialize the datepicker
+datePicker.datepicker();
+
 addBtn.addEventListener('click', () => {
-    
     if (input.value === '' || count === 1) {
-        alert('Please enter a task! or Choose date!');
+        alert('Please enter a task or choose a date!');
     } else {
         console.log(`list${count}`);
         const list = document.getElementById(`list${count}`);
-        
+
         const li = document.createElement('li');
         li.innerHTML = `${input.value} <button class="deleteBtn">Delete</button>`;
         list.appendChild(li);
@@ -28,26 +30,24 @@ addBtn.addEventListener('click', () => {
 });
 
 updateBtn.addEventListener('click', () => {
-    const date = new Date(datePicker.value);
-    if (isNaN(date)) {
+    const date = datePicker.datepicker('getDate');
+    if (!date) {
         alert('Please select a valid date!');
         return;
     }
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    const weekDiv = document.createElement('div'); // create new div element
-    console.log(weekDiv);
-    weekDiv.classList.add('week'); // add the 'week' class to the div
-   
-    const h2 = document.createElement('h2'); // create new unordered list element
-    h2.id = 'weekHeading'+(++count); // set the id of the list element to a new unique value
-    h2.innerText = date.toLocaleDateString(undefined, options);
-   
-    const ul = document.createElement('ul'); // create new unordered list element
-    ul.id = `list${count}`; // set the id of the list element to a new unique value
-    weekDiv.appendChild(h2); // add the list element to the new div element
-    weekDiv.appendChild(ul); // add the list element to the new div element
-    weekDiv.id='count'+count;
+    const weekDiv = document.createElement('div');
+    weekDiv.classList.add('week');
 
-    console.log(mainTable[0].innerHTML);
-    mainTable[0].appendChild(weekDiv); 
+    const h2 = document.createElement('h2');
+    h2.id = 'weekHeading' + (++count);
+    h2.innerText = date.toLocaleDateString(undefined, options);
+
+    const ul = document.createElement('ul');
+    ul.id = `list${count}`;
+    weekDiv.appendChild(h2);
+    weekDiv.appendChild(ul);
+    weekDiv.id = 'count' + count;
+
+    mainTable[0].appendChild(weekDiv);
 });

dad0d7477949363563823dfcc4eb9779b77f838d
Emirhan Kaya <emirhan.kaya@ug.bilkent.edu.tr>
2023-06-04 17:01:20 +0300

    wip

--- JavaScript/weeklyplanner.js
+++ JavaScript/weeklyplanner.js
@@ -1,7 +1,7 @@
-var count = 1;
+var weekCount = 1;
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
-const datePicker = $('#datePicker');
+const datePicker = $( "#datePicker" );
 const updateBtn = document.getElementById('updateBtn');
 const weekHeading = document.getElementById('weekHeading');
 const mainTable = document.getElementsByClassName('maintable');
@@ -11,11 +11,11 @@ const li = document.createElement('li');
 datePicker.datepicker();
 
 addBtn.addEventListener('click', () => {
-    if (input.value === '' || count === 1) {
+    if (input.value === '' || weekCount === 1) {
         alert('Please enter a task or choose a date!');
     } else {
-        console.log(`list${count}`);
-        const list = document.getElementById(`list${count}`);
+        console.log(`list${weekCount}`);
+        const list = document.getElementById(`list${weekCount}`);
 
         const li = document.createElement('li');
         li.innerHTML = `${input.value} <button class="deleteBtn">Delete</button>`;
@@ -30,7 +30,7 @@ addBtn.addEventListener('click', () => {
 });
 
 updateBtn.addEventListener('click', () => {
-    const date = datePicker.datepicker('getDate');
+    const date = new Date(datePicker.datepicker('getDate'));
     if (!date) {
         alert('Please select a valid date!');
         return;
@@ -40,14 +40,14 @@ updateBtn.addEventListener('click', () => {
     weekDiv.classList.add('week');
 
     const h2 = document.createElement('h2');
-    h2.id = 'weekHeading' + (++count);
+    h2.id = 'weekHeading' + (++weekCount);
     h2.innerText = date.toLocaleDateString(undefined, options);
 
     const ul = document.createElement('ul');
-    ul.id = `list${count}`;
+    ul.id = `list${weekCount}`;
     weekDiv.appendChild(h2);
     weekDiv.appendChild(ul);
-    weekDiv.id = 'count' + count;
+    weekDiv.id = 'count' + weekCount;
 
     mainTable[0].appendChild(weekDiv);
 });
